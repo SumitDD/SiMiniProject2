fdfa15baf06333d065e51e7a8e09b71195b22a43
Phillip <phillip_andersen@hotmail.com>
2022-10-05 10:59:12 +0200

    done

---
+++ SiMiniProject2BPM/MicroService2/DataAccess/ComplaintRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Common.Models;
+using Dapper;
+using Npgsql;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace MicroService2.DataAccess
+{
+    public class ComplaintRepository
+    {
+        private const string PostgresConnectionString =
+            "Server=localhost;Port=5432;User Id=postgres;Password=P@ris2027!;Database=postgres;";
+        private readonly IDbConnection _connection;
+
+        public ComplaintRepository()
+        {
+            _connection = new NpgsqlConnection("Server=localhost;Port=5432;User Id=postgres;Password=P@ris2027!;Database=postgres;");
+        }
+
+        public async Task<bool> SaveComplaint(Complaint complaint)
+        {
+            try
+            {
+                await _connection.ExecuteAsync(@"INSERT INTO complaint (email, complaintmessage) VALUES (@Email, @ComplaintMessage)",
+                    new { Email = complaint.Email, ComplaintMessage = complaint.Email });
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
--- SiMiniProject2BPM/MicroService2/Services/ComplaintReceiver.cs
+++ SiMiniProject2BPM/MicroService2/Services/ComplaintReceiver.cs
@@ -1,4 +1,6 @@
-﻿using Common.Models;
+﻿using System;
+using Common.Models;
+using MicroService2.DataAccess;
 using Newtonsoft.Json;
 using RedisLibrary.RedisConnector;
 
@@ -10,12 +12,16 @@ namespace MicroService2.Services
         {
             var connection = RedisConnector.GetConnection();
             var pubSub = connection.GetSubscriber();
-            pubSub.Subscribe("channel", async (channel, message)=> {
+            pubSub.Subscribe("channel", async (channel, message) =>
+            {
                 Console.WriteLine(message);
                 var deserializedComplaint = JsonConvert.DeserializeObject<Complaint>(message);
+                // Save the complaint
+                var repo = new ComplaintRepository();
+                await repo.SaveComplaint(deserializedComplaint);
 
                 // send email
-                await EmailService.SendEmailComplaintResponse(new ComplaintResponse { Email = deserializedComplaint.Email});
+                await EmailService.SendEmailComplaintResponse(new ComplaintResponse { Email = deserializedComplaint.Email });
             });
 
             Console.ReadLine();
